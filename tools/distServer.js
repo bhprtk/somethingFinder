70e2c263c4d4f0ac555577a2de99f4fadd8d0f68
pratik <lazy.monk.13@gmail.com>
2016-10-28 14:26:00 -0700

    remove dotenv for production 😓

--- tools/distServer.js
+++ tools/distServer.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import compression from 'compression';
-require('dotenv').load();
 
 import bodyParser from 'body-parser';
 
