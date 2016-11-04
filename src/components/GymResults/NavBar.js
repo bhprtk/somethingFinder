e35f4938daeaa9f6c8af3a4448b032b38828a2f2
pratik <lazy.monk.13@gmail.com>
2016-11-04 11:27:37 -0700

    ready to deploy

--- src/components/GymResults/NavBar.js
+++ src/components/GymResults/NavBar.js
@@ -10,10 +10,19 @@ class NavBar extends Component {
 		return (
 			<nav className="navbar navbar-default" style={styles.navbar}>
 				<div className="container-fluid">
-					<Link to="/" className="navbar-brand">
+					<Link to="/" className="navbar-brand pull-left">
 						<img src="http://www.iconsdb.com/icons/preview/white/dumbbell-xxl.png" className="dumbell-image"/>
 						<span style={styles.brand}>Find Gyms</span>
 					</Link>
+					<div className="pull-right">
+						<a
+							target="_blank"
+							href="https://github.com/bhprtk/somethingFinder"
+							style={styles.github}
+							role="button">
+							<span className="fa fa-github fa-4x"></span>
+						</a>
+					</div>
 					</div>
 			</nav>
 		);
@@ -30,6 +39,9 @@ const styles = {
 		color: "#fff",
 		fontSize: 30,
 		margin: 0
+	},
+	github: {
+		color: "#fff"
 	}
 };
 
